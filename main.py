2efbd5c4d8375cff22d5aeb37c17822159ddd858
Ratius9919 <91630194+Ratius9919@users.noreply.github.com>
2021-10-07 18:38:02 -0300

    Add files via upload
    Alta de pacientes

--- main.py
+++ main.py
@@ -1,41 +1,19 @@
-def crear_matriz(n, m):
-    return [[0] * m for fila in range(n)]
+def crear_lista():
+    lista_pacientes = []
+    return lista_pacientes
+
+def validar_edad(edad):
+    while True:
+        if edad < 17:
+            break
 
 
-def llenar_matriz(m): #Llenamos la matriz creada de listas dentro de cada casillero
-    filas = len(m)
-    columnas = len(m[0])
-    for f in range(filas):
-        for c in range(columnas):
-            m[f][c] = lista = []
 
-
-def mostrar_matriz(m): #Imprimimos la fila junto a la cadena "Mes" y su respectivo nro
-    a = 1
-    for fila in m:
-        print("Mes", a, fila, ":")
-        a += 1
-
-
-def ajustar_meses(m): #Ajustamos los dias del mes usando rebanadas.
-    filas = len(m)
-    columnas = len(m[0])
-    a = 0
-    for f in range(filas):
-
-        if f == 1:
-            febrero = m[f][1:29]
-            m[f] = febrero
-        elif f == 3 or f == 5 or f == 8 or f == 10:
-            mes = m[f][1:30]
-            m[f] = mes
-
-
-meses = 12
-dias = 31
-turnos_meses_dias = crear_matriz(12, dias)
-llenar_matriz(turnos_meses_dias)
-
-ajustar_meses(turnos_meses_dias)
-
-mostrar_matriz(turnos_meses_dias)
+def alta(l = crear_lista()):
+    a = int(input("Desea dar de alta un paciente? 0 = Si / 1 = no"))
+    while a == 0:
+        dni = input("Ingrese su DNI por favor.")
+        nombre = input("Ingrese su nombre por favor.")
+        edad = int(input("Cual es su edad"))
+        validar_edad(edad)
+        l.append(dni,nombre,edad)
