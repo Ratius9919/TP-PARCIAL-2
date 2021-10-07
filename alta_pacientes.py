a5f577a73980cef02bda1fa8c599ecd7910ec465
Ratius9919 <91630194+Ratius9919@users.noreply.github.com>
2021-10-07 19:11:50 -0300

    Update alta_pacientes.py

--- alta_pacientes.py
+++ alta_pacientes.py
@@ -2,18 +2,23 @@ def crear_lista():
     lista_pacientes = []
     return lista_pacientes
 
-def validar_edad(edad):
-    while True:
-        if edad < 17:
-            break
+
+def verificar_digitos(valor):
+    while valor.isdigit() == False:
+        print("ERRRRRRORRRR TIENEN QUE SER NUMEROS")
+        valor = input("Ingrese sus digitos nuevamente por favor.")
 
 
-
-def alta(l = crear_lista()):
+def alta(l=crear_lista()):
     a = int(input("Desea dar de alta un paciente? 0 = Si / 1 = no"))
     while a == 0:
         dni = input("Ingrese su DNI por favor.")
+        verificar_digitos(dni)
         nombre = input("Ingrese su nombre por favor.")
-        edad = int(input("Cual es su edad"))
-        validar_edad(edad)
-        l.append(dni,nombre,edad)
+
+        edad = input("Cual es su edad")
+        verificar_digitos(edad)
+
+        l.append(dni + "/" + nombre + "/" + edad)
+        a = int(input("Desea dar de alta un paciente? 0 = Si / 1 = no"))
+    return l
